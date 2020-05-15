0accf0b7be3631b16c0c51b67d33a07ee9529c50
unknown <elizabethchu18@gmail.com>
2020-05-14 22:58:43 -0700

    add album carousel

--- javascripts/carousel.js
+++ javascripts/carousel.js
@@ -1,7 +1,61 @@
-var songs = document.querySelectorAll("album-art")
 
-var currentIdx = songs.length/2
+let leftSide, current, rightSide;
 
-var leftSide = currentIdx === 0 ? songs[0] : songs.slice(0,currentIdx);
-var current = songs[currentIdx];
-var rightSide = currentIdx === songs.length-1 ? songs[songs.length-1] : songs.slice(currentIdx+1);
+var songNodes = document.querySelectorAll(".album-art")
+var songs = Array.from(songNodes).map((song) => song)
+var currentIdx = currentIdx || Math.floor(songs.length/2);
+
+updateCarousel();
+
+
+function animateAlbumCarousel(direction){
+        if (direction === 'previous') {
+            $(songs[currentIdx]).removeClass('centered-album')
+            currentIdx = (currentIdx - 1) % songs.length
+        } else if (direction ==='next') {
+            $(songs[currentIdx]).removeClass('centered-album')
+            currentIdx = (currentIdx + 1) % songs.length
+        }
+        updateCarousel();
+}
+
+
+function updateCarousel(){
+        var leftSide = currentIdx === 0 ? songs[0] : songs.slice(0,currentIdx);
+        var current = songs[currentIdx];
+        var rightSide = currentIdx === songs.length-1 ? songs[songs.length-1] : songs.slice(currentIdx+1);
+
+
+
+
+        $(current).removeClass('rotate-left-album rotate-right-album').addClass('centered-album')
+
+        for (let i=0; i < leftSide.length; i++) {
+            $(leftSide[i]).css("z-index", i)
+        }
+
+        for (let j=rightSide.length-1; j > 0; j--) {
+            $(rightSide[j]).css("z-index", -1 *j)
+        }
+
+        leftSide.forEach(element => {
+            $(element).addClass("rotate-left-album")
+            
+            var children = Object.values(element.children);
+
+            for (let i=0; i < children.length; i++) {
+                if (!$(children[i]).is('img')) $(children[i]).hide()
+            }
+        });
+
+        rightSide.forEach(element => {
+            $(element).addClass("rotate-right-album")
+
+            var children = Object.values(element.children);
+
+            for (let i=0; i < children.length; i++) {
+                if (!$(children[i]).is('img')) $(children[i]).hide()
+            }
+        });
+
+}
